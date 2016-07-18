0b9f0999e1cc8d74430b86fbfb3c9b6d5086a35b
Behzad Khosravifar <930919@dbitabriz.local>
2016-07-18 16:34:34 +0430

    first init

---
+++ src/WinServer/IISExpress.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace WinServer
+{
+    public static class IISExpress
+    {
+        private static readonly List<string> sites = new List<string>();
+        private static readonly List<string> paths = new List<string>();
+
+        public static void StartIISExpress(string site, int port = 7329)
+        {
+            if (!sites.Contains(site.ToLower()))
+                sites.Add(site.ToLower());
+            else return;
+
+            var index = Environment.CurrentDirectory.LastIndexOf("\\bin\\");
+            var projectDir = Environment.CurrentDirectory.Remove(index);
+            var solutionDir = System.IO.Directory.GetParent(projectDir);
+            var path = solutionDir + "\\" + site;
+
+            var arguments = new StringBuilder();
+            arguments.Append(@"/path:");
+            arguments.Append(path);
+            arguments.Append(@" /Port:" + port);
+            // arguments.Append(@"/site:" + site);
+            var process = Process.Start(new ProcessStartInfo()
+            {
+                FileName = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\IIS Express\\iisexpress.exe",
+                Arguments = arguments.ToString(),
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+        }
+
+
+        public static void StartIISExpressFromPath(string path, int port = 7329)
+        {
+            if (!paths.Contains(path.ToLower()))
+                paths.Add(path.ToLower());
+            else return;
+
+            var arguments = new StringBuilder();
+            arguments.Append(@"/path:" + path);
+            arguments.Append(@" /Port:" + port);
+            var process = Process.Start(new ProcessStartInfo()
+            {
+                FileName = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\IIS Express\\iisexpress.exe",
+                Arguments = arguments.ToString(),
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+        }
+    }
+}
---
+++ src/WinServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinServer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var dir = Environment.CurrentDirectory;
+            var server = new ProcessStartInfo(@"C:\Program Files (x86)\IIS Express\iisexpress.exe", $@"/port:8001 /path:{dir}\Publish");
+            server.CreateNoWindow = true;
+            server.UseShellExecute = false;
+            server.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process _process;
+            _process = Process.Start(server);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+            _process.Kill();
+        }
+
+
+    }
+}
